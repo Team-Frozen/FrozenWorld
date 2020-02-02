fddc20dc844e95df9ba06fd26f5d90f1d3562fdc
parkkm <kio1015@naver.com>
2020-02-02 18:44:35 +0900

    UI outline

---
+++ GamePlay/Assets/Scripts/Exit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    RaycastHit hit;
+    [SerializeField] Player player;
+    [SerializeField] StageManager stageManager;
+    int layerMask_player;
+
+    void Start()
+    {
+        layerMask_player = 1 << LayerMask.NameToLayer("Player");
+    }
+
+    void Update()
+    {
+        //Player의 이동방향이 Exit로 향할 때
+        if (player.direction == Vector3.back)
+        {
+            //Player와 Exit가 충돌했을 때, StageClear Panel 띄움
+            if (Physics.Raycast(transform.position, Vector3.forward, out hit, 0.6f, layerMask_player))
+            {
+                stageManager.ShowStageClearUI();
+            }
+        }
+    }    
+}
---
+++ GamePlay/Assets/Scripts/StageManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageManager : MonoBehaviour
+{
+    [SerializeField] GameObject go_StageClearUI;
+    [SerializeField] Text txt_playerMoves;
+    static public int playerMoves;
+    static public int currentStage;
+
+    [SerializeField] GameObject[] go_Stages;
+    //[SerializeField] Transform[] tf_PlayerPos;
+    //[SerializeField] Rigidbody rigid_Player;
+    //int[] minimumMoves;
+    //int[] score;
+    
+    void Start()
+    {
+        ShowNewStage(currentStage);
+    }
+
+    void Update()
+    {
+        txt_playerMoves.text = playerMoves.ToString();
+    }
+
+    public void ShowNewStage(int stageIndex)
+    {
+        Player.canMove = true;
+        currentStage = stageIndex;
+        go_Stages[currentStage - 1].SetActive(true);
+        go_StageClearUI.SetActive(false);
+        playerMoves = 0;
+    }
+    
+    public void ShowStageClearUI()
+    {
+        Player.canMove = false;
+        go_StageClearUI.SetActive(true);
+        //하트 띄우기, 하트 개수 저장 추가해야 함
+    }
+
+    public void NextStageBtn()
+    {
+        if (currentStage < go_Stages.Length)
+        {
+            Player.canMove = true;
+            //rigid_Player.gameObject.transform.position = tf_PlayerPos[currentStage].position;
+            currentStage -= 1;
+            go_Stages[currentStage++].SetActive(false);
+            go_Stages[currentStage++].SetActive(true);
+            go_StageClearUI.SetActive(false);
+            playerMoves = 0;
+        }
+    }
+}
