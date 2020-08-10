15d39534d84aa7f72f8f9477b36861981be42dfc
Lee JJ <2wjdwo97@naver.com>
2020-08-10 22:57:32 +0900

    GamePlay *1st floor StpBlock img fix

--- GamePlay/Assets/Scripts/Elements/BlockStop.cs
+++ GamePlay/Assets/Scripts/Elements/BlockStop.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BlockStop : Element
 {
     private void Awake()
     {
         if (this.transform.position.y > 1)
-            this.transform.localScale = new Vector3(0.6f, 0, 0.6f);
+            this.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load("platform/block/StopBlock2", typeof(Sprite));
     }
 
     void OnTriggerEnter(Collider other)
